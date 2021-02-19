2fb8a6b5335b52a3fd258a9ba03bb80595d6a676
hideaki-umizaru <momoirosoccer@gmail.com>
2021-02-19 15:35:08 +0900

    完成

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,6 @@
 
 <script>
 import axios from "axios";
-
 export default {
   data() {
     return {
@@ -18,13 +17,12 @@ export default {
   },
   methods: {
     sendPost() {
-      axios.get(
-          `https://apis.postcode-jp.com/api/v4/postcodes/${this.getNumber}?apiKey=yO7TZrzN6bwf5fryzBCn5dXB7Llq4JhTtL0DnaH` +
-            this.getNumber)
-        .then((res) => {
-          this.show = res.data;
+      axios.get(`https://apis.postcode-jp.com/api/v4/postcodes/${this.getNumber}?apiKey=yO7TZrzN6bwf5fryzBCn5dXB7Llq4JhTtL0DnaH`)
+      .then((res) => {
+          this.show = res.data[0].allAddress;
+          console.log(res.data[0].allAddress);
         });
-      console.log(res);
+  
     },
   },
 };
