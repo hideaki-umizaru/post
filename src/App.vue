e2a1e0652357d45e7fc5c329228359abfc999d7c
hideaki-umizaru <momoirosoccer@gmail.com>
2021-02-19 09:14:23 +0900

    3回目

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-  <input type="number" placeholder="000-0000" v-model="getNumber">
-  <button @click="sendPost">住所自動入力</button>
-  <h2>Address:{{show}}</h2>
+    <input type="number" placeholder="000-0000" v-model="getNumber" />
+    <button @click="sendPost">住所自動入力</button>
+    <h2>Address:{{ show }}</h2>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-const axios = require('axios');
 
 export default {
-  data () {
+  data() {
     return {
-      getNumber:"",
-      show:""
+      getNumber: "",
+      show: "",
     };
   },
-  methods:{
-  sendPost() { 
-  　const response = await 
-    axios.get(`https://apis.postcode-jp.com/api/v4/postcodes/0123456?apiKey=yO7TZrzN6bwf5fryzBCn5dXB7Llq4JhTtL0DnaH`);
-    console.log(response);
-  }
-  }
+  methods: {
+    sendPost() {
+      const response = await;
+      axios
+        .get(
+          `https://apis.postcode-jp.com/api/v4/postcodes/0123456?apiKey=yO7TZrzN6bwf5fryzBCn5dXB7Llq4JhTtL0DnaH` +
+            this.getNumber
+        )
+        .then((res) => {
+          this.show = res.data;
+        });
+      console.log(response);
+    },
+  },
 };
 </script>
